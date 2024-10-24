1a559c61883d3410b6c6c5f39c9f553e287937aa
LuchoBre <104022090+luchobre@users.noreply.github.com>
2024-10-24 14:40:06 -0300

    Create funciones.h

---
+++ funciones.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int tirarDado();
+void mostrarDados(const vector<int>& dados);
+int jugarRonda();
+void onePlayer();
+void twoPlayers();
+int menu();
+void options(int opc);
